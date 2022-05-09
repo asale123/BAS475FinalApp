603754293417cf581e9453d404590413a4425ba5
Andrew Sale <99213962+asale123@users.noreply.github.com>
2022-05-08 20:58:16 -0400

    Adding my final app to my repository.

---
+++ server.R
@@ -0,0 +1,241 @@
+library(shiny)
+library(shinyWidgets)
+library(quantmod)
+library(plotly)
+library(DT)
+library(shinythemes)
+library(shinydashboard)
+library(dygraphs)
+library(ggplot2)
+library(fpp3)
+
+RETAIL <- aus_retail %>%
+  mutate(Month = yearmonth(Month)) %>% 
+  update_tsibble(index = Month) %>% 
+  as_tsibble(index = Month, regular = TRUE) 
+
+server <- shinyServer(function(input, output) {
+  
+  output$plot1 <- renderPlot({
+    {
+      aus_retail %>% 
+        filter(Month >= input$date[1] & Month <= input$date[2]) %>% 
+        filter(State == input$state, Industry == input$industry) %>%
+        mutate(Month = yearmonth(Month)) %>% 
+        update_tsibble(index = Month) %>% 
+        as_tsibble(index = Month, regular = TRUE) %>% 
+        mutate(diff = difference(Turnover)) %>%
+        autoplot()
+    }
+  }) 
+  
+  output$interp <- renderPrint({
+    if(input$diff == "Seasonality"){
+      "This graph is looking at seasonality. When looking at these graphs if the means are relatively the same, meaning that the blue line on the graph is at the same height on all of the graphs, then there is weak seasonality. If there is a large difference between the blue lines of the graphs then that shows strong seasonality."} 
+    else if(input$diff == "Autocorrelation"){
+      "This graph is looking at Autocorrelation using the ACF. This shows the linear relationship between lagged values of a time series. When looking at this graph the dashed blue lines show if the correlations are significantly greater than zero. If the black lines do not go past the blue lines then there is a weak seasonal pattern. If the lines exceed the blue dashed lines there is usually a pattern and it means that seasonality exists. An example of this is a peak every 12 months shown well through the department store input."} 
+    else{
+      "This graph is looking at White Noise. When looking at these plots, you want the residual plot to have homoscedasticity, meaning equal spread throughout. When looking at the ACF plot you want the lines to stay inside of the blue dashed lines. Finally when looking at the histogram, you want it to look normal, meaning that it has a bell shaped curve."
+    }
+  })
+  
+  output$interp2 <- renderPrint({
+    if(input$dec == "Additive"){
+      "This graph is looking at the Classical Additive Decomposition. The first graph shows the regular plot of the data. The secons plot shows the trend of the data, so when the graph goes up there is an increasing trend, while if it goes down there is a decreasing trend. The next plot shows seasonality, so if there is a pattern of increases and decreases that means that you have seasonality. Finally the last plot shows the remainder, or what is left over. When looking at this plot you want the dime, the grey bar at the begenning of the graph to be very large, and you want the other dimes to be very small. When this happens it means that most of the variation is explained through the trend and seasonality which is a great thing."} 
+    else{
+      "This graph is looking at the Classical Multiplicative Decomposition. The first graph shows the regular plot of the data. The secons plot shows the trend of the data, so when the graph goes up there is an increasing trend, while if it goes down there is a decreasing trend. The next plot shows seasonality, so if there is a pattern of increases and decreases that means that you have seasonality. Finally the last plot shows the remainder, or what is left over. When looking at this plot you want the dime, the grey bar at the begenning of the graph to be very large, and you want the other dimes to be very small. When this happens it means that most of the variation is explained through the trend and seasonality which is a great thing."
+    }
+  })
+  
+  output$plot2 <- renderPlot({
+    if(input$diff == "Seasonality"){
+      aus_retail %>%
+        filter(Month >= input$date[1] & Month <= input$date[2]) %>% 
+        filter(State == input$state, Industry == input$industry) %>%
+        mutate(Month = yearmonth(Month)) %>% 
+        update_tsibble(index = Month) %>% 
+        as_tsibble(index = Month, regular = TRUE) %>% 
+        mutate(diff = difference(Turnover)) %>% 
+        gg_subseries() +
+        labs(title = "Sesonality using a Subseries Plot of Australia's Retail Turnover")
+    } else if (input$diff == "Autocorrelation"){
+      aus_retail %>%
+        filter(Month >= input$date[1] & Month <= input$date[2]) %>% 
+        filter(State == input$state, Industry == input$industry) %>%
+        mutate(Month = yearmonth(Month)) %>% 
+        update_tsibble(index = Month) %>% 
+        as_tsibble(index = Month, regular = TRUE) %>% 
+        mutate(diff = difference(Turnover)) %>% 
+        ACF(diff) %>% 
+        autoplot() +
+        labs(title = "Autocorrelation using a Correlogram of Australia's Retail Turnover")
+    } else{
+      aus_retail %>%
+        filter(Month >= input$date[1] & Month <= input$date[2]) %>% 
+        filter(State == input$state, Industry == input$industry) %>%
+        mutate(Month = yearmonth(Month)) %>% 
+        update_tsibble(index = Month) %>% 
+        as_tsibble(index = Month, regular = TRUE) %>% 
+        mutate(diff = difference(Turnover)) %>% 
+        model(NAIVE = MEAN(diff)) %>% 
+        gg_tsresiduals() +
+        labs(title = "White Noise Plots of Australia's Retail Turnover")
+    } 
+    
+  })
+  
+  output$plot3 <- renderPlot({
+    if(input$dec == "Additive"){
+      aus_retail %>%
+        filter(Month >= input$date[1] & Month <= input$date[2]) %>% 
+        filter(State == input$state, Industry == input$industry) %>%
+        model(classical_decomposition(Turnover, type = "additive")) %>%
+        components() %>%
+        autoplot() +
+        labs(title = "Classical Additive Decomposition of Australia's Retail Turnover")
+    } else {
+      aus_retail %>%
+        filter(State == input$state, Industry == input$industry) %>%
+        model(classical_decomposition(Turnover, type = "multiplicative")) %>%
+        components() %>%
+        autoplot() +
+        labs(title = "Classical Multiplicative Decomposition of Australia's Retail Turnover")
+    }
+  })
+  
+  output$plot4 <- renderPlot({
+    if(input$basic == "NAIVE"){
+      RETAIL %>% 
+        filter(Month >= input$date[1] & Month <= input$date[2]) %>% 
+        filter(State == input$state, Industry == input$industry) %>%
+        model(Naive = NAIVE(Turnover)) %>% 
+        forecast(h = input$forecast, level=95) %>%
+        autoplot(RETAIL %>% filter(Month >= input$date[1] & Month <= input$date[2])) +
+        labs(title = "NAIVE forecast using a Subseries Plot of Australia's Retail Turnover")
+    } else if (input$basic == "Seasonal NAIVE"){
+      RETAIL %>% 
+        filter(Month >= input$date[1] & Month <= input$date[2]) %>% 
+        filter(State == input$state, Industry == input$industry) %>%
+        model(SNAIVE = SNAIVE(Turnover)) %>% 
+        forecast(h = input$forecast) %>%
+        autoplot(RETAIL %>% filter(Month >= input$date[1] & Month <= input$date[2])) +
+        labs(title = "Seasonal NAIVE forecast using a Subseries Plot of Australia's Retail Turnover")
+    } else if (input$basic == "Drift"){
+      RETAIL %>% 
+        filter(Month >= input$date[1] & Month <= input$date[2]) %>% 
+        filter(State == input$state, Industry == input$industry) %>%
+        model(Drift = RW(Turnover ~ drift())) %>% 
+        forecast(h = input$forecast) %>%
+        autoplot(RETAIL %>% filter(Month >= input$date[1] & Month <= input$date[2])) +
+        labs(title = "Drift forecast using a Subseries Plot of Australia's Retail Turnover")
+    } else if (input$basic == "Linear"){
+      RETAIL %>% 
+        filter(Month >= input$date[1] & Month <= input$date[2]) %>% 
+        filter(State == input$state, Industry == input$industry) %>%
+        model(Linear = TSLM(Turnover ~ trend())) %>% 
+        forecast(h = input$forecast) %>%
+        autoplot(RETAIL %>% filter(Month >= input$date[1] & Month <= input$date[2])) +
+        labs(title = "Linear forecast using a Subseries Plot of Australia's Retail Turnover")
+    } else{
+      RETAIL %>% 
+        filter(Month >= input$date[1] & Month <= input$date[2]) %>% 
+        filter(State == input$state, Industry == input$industry) %>%
+        model(Mean = MEAN(Turnover)) %>% 
+        forecast(h = input$forecast) %>%
+        autoplot(RETAIL %>% filter(Month >= input$date[1] & Month <= input$date[2])) +
+        labs(title = "Mean forecast using a Subseries Plot of Australia's Retail Turnover")
+    } 
+    
+  })
+  
+  
+  output$plot5 <- renderPlot({
+    if(input$ETS == "Holts"){
+      RETAIL %>% 
+        filter(Month >= input$date[1] & Month <= input$date[2]) %>% 
+        filter(State == input$state, Industry == input$industry) %>%
+        model(additive = ETS(Turnover ~ error("A") + trend("A") +
+                               season("A"))) %>% 
+        forecast(h = input$forecast) %>%
+        autoplot(RETAIL %>% filter(Month >= input$date[1] & Month <= input$date[2])) +
+        labs(title = "Exponential Smoothing forecast using Holts' as a Subseries Plot of Australia's Retail Turnover")
+    } else {
+      RETAIL %>% 
+        filter(Month >= input$date[1] & Month <= input$date[2]) %>% 
+        filter(State == input$state, Industry == input$industry) %>%
+        model(multiplicative = ETS(Turnover ~ error("M") + trend("A") +
+                               season("M"))) %>% 
+        forecast(h = input$forecast) %>%
+          autoplot(RETAIL %>% filter(Month >= input$date[1] & Month <= input$date[2])) +
+          labs(title = "Exponential Smoothing forecast using Holt's Winters as a Subseries Plot of Australia's Retail Turnover")
+    }
+  })
+
+  output$plot6 <- renderPlot({
+    if(input$arima == "ARIMA210"){
+      RETAIL %>% 
+        filter(Month >= input$date[1] & Month <= input$date[2]) %>% 
+        filter(State == input$state, Industry == input$industry) %>%
+        model(arima210 = ARIMA(Turnover ~ pdq(2,1,0))) %>% 
+        forecast(h = input$forecast) %>%
+        autoplot(RETAIL %>% filter(Month >= input$date[1] & Month <= input$date[2])) +
+        labs(title = "ARIMA forecast with the values of pdq(2,1,0) using a Subseries Plot of Australia's Retail Turnover")
+    } else if (input$arima == "ARIMA013"){
+      RETAIL %>% 
+        filter(Month >= input$date[1] & Month <= input$date[2]) %>% 
+        filter(State == input$state, Industry == input$industry) %>%
+        model(arima013 = ARIMA(Turnover ~ pdq(0,1,3))) %>% 
+        forecast(h = input$forecast) %>%
+        autoplot(RETAIL %>% filter(Month >= input$date[1] & Month <= input$date[2])) +
+        labs(title = "ARIMA forecast with the values of pdq(0,1,3) using a Subseries Plot of Australia's Retail Turnover")
+    } else if (input$arima == "ARIMA000011"){
+      RETAIL %>% 
+        filter(Month >= input$date[1] & Month <= input$date[2]) %>% 
+        filter(State == input$state, Industry == input$industry) %>%
+        model(arima000011 = ARIMA(Turnover ~ pdq(0,0,0) + PDQ(0,1,1))) %>% 
+        forecast(h = input$forecast) %>%
+        autoplot(RETAIL %>% filter(Month >= input$date[1] & Month <= input$date[2])) +
+        labs(title = "ARIMA forecast with seasonal componet with the values of pdq(0,0,0), PDQ(0,1,1) using a Subseries Plot of Australia's Retail Turnover")
+    } else if (input$arima == "ARIMA210011"){
+      RETAIL %>% 
+        filter(Month >= input$date[1] & Month <= input$date[2]) %>% 
+        filter(State == input$state, Industry == input$industry) %>%
+        model(arima210011 = ARIMA(Turnover ~ pdq(2,1,0) + PDQ(0,1,1))) %>% 
+        forecast(h = input$forecast) %>%
+        autoplot(RETAIL %>% filter(Month >= input$date[1] & Month <= input$date[2])) +
+        labs(title = "ARIMA forecast with seasonal componet with the values of pdq(2,1,0), PDQ(0,1,1) using a Subseries Plot of Australia's Retail Turnover")
+    } else if (input$arima == "Stepwise"){
+      RETAIL %>% 
+        filter(State == input$state, Industry == input$industry) %>%
+        model(stepwise = ARIMA(Turnover)) %>% 
+        forecast(h = input$forecast) %>%
+        autoplot(RETAIL %>% filter(Month >= input$date[1] & Month <= input$date[2])) +
+        labs(title = "ARIMA Stepwise forecast using a Subseries Plot of Australia's Retail Turnover")
+    } else if (input$arima == "Auto"){
+      RETAIL %>% 
+        filter(Month >= input$date[1] & Month <= input$date[2]) %>% 
+        filter(State == input$state, Industry == input$industry) %>%
+        model(auto = ARIMA(Turnover, stepwise = FALSE, approx = FALSE)) %>% 
+        forecast(h = input$forecast) %>%
+        autoplot(RETAIL %>% filter(Month >= input$date[1] & Month <= input$date[2])) +
+        labs(title = "Auto ARIMA forecast using a Subseries Plot of Australia's Retail Turnover")
+    } else{
+      RETAIL %>% 
+        filter(Month >= input$date[1] & Month <= input$date[2]) %>% 
+        filter(State == input$state, Industry == input$industry) %>%
+        model(arima = ARIMA(Turnover ~ pdq(input$p,input$d,input$q))) %>% 
+        forecast(h = input$forecast) %>%
+        autoplot(RETAIL %>% filter(Month >= input$date[1] & Month <= input$date[2])) +
+        labs(title = "Your own ARIMA forecast using a Subseries Plot of Australia's Retail Turnover")
+    
+    } 
+    
+  })
+})
+
+
+
+
+
+
+
---
+++ ui.R
@@ -0,0 +1,192 @@
+library(shiny)
+library(shinyWidgets)
+library(quantmod)
+library(plotly)
+library(DT)
+library(shinythemes)
+library(shinydashboard)
+library(dygraphs)
+library(ggplot2)
+library(fpp3)
+library(dashboardthemes)
+library(shinythemes)
+library(rsconnect)
+rsconnect::setAccountInfo(name='andrewasale3',
+                          token='DCE012E5AC85A047CD44BF88A07F4940',
+                          secret='otSyLevgBv70aPlJMQarqZBecDSEUoqgSxWYnhil')
+rsconnect::deployApp('path/to/your/app')
+
+
+data("aus_retail")
+retail <- aus_retail
+RETAIL <- aus_retail %>%
+  mutate(Month = yearmonth(Month)) %>% 
+  update_tsibble(index = Month) %>% 
+  as_tsibble(index = Month, regular = TRUE)
+
+train <- RETAIL %>% 
+  filter(Month <= yearmonth("2005 Jan"))
+
+holdout <- RETAIL %>% 
+  filter(Month > yearmonth("2005 Jan"))
+
+
+ui <- dashboardPage(dashboardHeader(title="Exploring Australian Retail"), 
+                    dashboardSidebar(id="",sidebarMenu(
+                      menuItem(menuSubItem("Instructions",tabName = "Inst")),
+                      menuItem(menuSubItem(text="Australian Stock Market News", icon = icon("send",lib='glyphicon'), href = "https://www.businessnewsaustralia.com/")),
+                      menuItem(menuSubItem("Choose Your Item",tabName = "Ind")),
+                      menuItem(text="Let's plot!",icon=icon("chart-line"),
+                               menuSubItem("Plotting!",tabName = "ST1"),
+                               menuSubItem("Decomposition",tabName = "ST2")),
+                      menuItem(text = "Let's Forecast!", icon = icon("chart-line"),
+                               menuSubItem("Forecast Length?", tabName = "ST3"),
+                               menuSubItem("Basic Models!",tabName = "ST4"),
+                               menuSubItem("ETS",tabName = "ST5"),
+                               menuSubItem("ARIMA",tabName = "ST6")))),
+                    
+                    dashboardBody(tabItems(
+                      tabItem(
+                        tabName = "Inst",
+                        fluidPage(
+                          theme = shinytheme("journal"),
+                          tags$head(tags$style(HTML('
+        /* logo */
+        .skin-blue .main-header .logo {
+                              background-color: #f77f00;
+                              }
+
+        /* logo when hovered */
+        .skin-blue .main-header .logo:hover {
+                              background-color: #ffffff;
+                              }
+
+        /* navbar (rest of the header) */
+        .skin-blue .main-header .navbar {
+                              background-color: #f77f00;
+                              }        
+
+        /* main sidebar */
+        .skin-blue .main-sidebar {
+                              background-color: #f77f00;
+                              }
+
+        /* active selected tab in the sidebarmenu */
+        .skin-blue .main-sidebar .sidebar .sidebar-menu .active a{
+                              background-color: #f77f00;
+                              }
+
+        /* other links in the sidebarmenu */
+        .skin-blue .main-sidebar .sidebar .sidebar-menu a{
+                              background-color: #f77f00;
+                              color: #000000;
+                              }
+
+        /* other links in the sidebarmenu when hovered */
+         .skin-blue .main-sidebar .sidebar .sidebar-menu a:hover{
+                              background-color: #ffffff;
+                              }
+        /* toggle button when hovered  */                    
+         .skin-blue .main-header .navbar .sidebar-toggle:hover{
+                              background-color: #ffffff;
+                              }
+                              '))),
+                          h1("Welcome to my app! Through my app you will be exploring the Austrailian Retail Industry", align = "center"),
+                          h3("Once you click on the \"Australian Stock Market News\" tab, you will be redirected to a web page that shows the latest Australian Stock Market News", align = "center"),
+                          h3("Under the \"Choose Your Item\" tab you will be able to pick the Australian Region, along with being able to chose the retail industry that you want to explore. You will also be able to see the date range that you want to see. Once you chose your preferences the graph will change to show your choice over time.", align = "center"),
+                          h3("Under the \"Let's Plot!\" tab you will be able to pick plotting many types of graphs including Seasonality, AutoCorelation, White Noise, and both Additive and Multiplicative Decomposition", align = "center"),
+                          h3("Under the \"Plotting\" sub tab you will be able to pick between Seasonality, AutoCorelation, and White Noise. You have to click on one of the choices to see the graph. Whenever you want to see the next plot you have to click on that one that you previously clicked then click on the graph that you want to see next", align = "center"),
+                          h3("Under the \"Decomposition\" sub tab you will be able to pick between Additive and Multiplicative Decomposition. You have to click on one of the choices to see the graph. Whenever you want to see the next plot you have to click on that one that you previously clicked then click on the graph that you want to see next", align = "center"),
+                          h3("Under the \"Let's Forecast!\" tab you will be able to forecast through many techniques", align = "center"),
+                          h3("Under the \"Forecast Length?\" tab you can use the slider input to choose how many months in the future that you would like to forecast", align = "center"),
+                          h3("Under the \"Basic Models!\" sub tab you will  be able to look at the NAIVE, Seasonal NAIVE, Drift, Linear, and the MEAN models of forecasting.", align = "center"),
+                          h3("Under the \"ETS\" sub tab you will be using Exponential Smoothing technique of both Holt's and Holt's Winters to get your forecast", align = "center"),
+                          h3("Under the \"ARIMA\" sub tab you will be using ARIMA models to get your forecast. There will be pre-made ARIMA models like the ARIMA210, but there will also be an AUTO ARIMA model and a Stepwise ARIMA Model. FInally you will also be able to make your own ARIMA model selecting the pdq through the number input and selecting \"ARIMA\"", align = "center")
+                        )),
+                      
+                      tabItem(
+                        tabName = "Ind",
+                        selectInput(
+                          "industry",
+                          h3("Select an industry"),
+                          choices = names(table(retail$Industry)),
+                          selected = 1
+                        ),
+                        fluidRow(column(3, verbatimTextOutput("value"))),
+                        selectInput(
+                          "state",
+                          h3("Select an Australian State"),
+                          choices = names(table(retail$State)),
+                          selected = 1
+                        ),
+                        fluidRow(column(3, verbatimTextOutput("value2"))),
+                        dateRangeInput("date", "Filter by Month and Year:",
+                                       start  = "1982-05-01",
+                                       end    = "2018-12-31",
+                                       min    = "1982-05-01",
+                                       max    = "2018-12-31",
+                                       format = "mm/yyyy"),
+                        plotOutput(outputId = "plot1")
+                      ),
+                      tabItem(tabName = "ST1",
+                              checkboxGroupButtons(
+                                inputId = "diff",
+                                label = "Choices", 
+                                choices = c("Seasonality", "Autocorrelation", "White Noise"),
+                                selected = "Seasonality",
+                                status = "danger"
+                              ),
+                              plotOutput(outputId = "plot2"),
+                              verbatimTextOutput("interp"
+                              )),
+                      tabItem(tabName = "ST2",
+                              checkboxGroupButtons(
+                                inputId = "dec",
+                                label = "Choices", 
+                                choices = c("Additive", "Multiplicative"),
+                                selected = "Additive",
+                                status = "danger"
+                              ),
+                              plotOutput(outputId = "plot3"),
+                              verbatimTextOutput("interp2"
+                              )),
+                      tabItem(tabName = "ST3",
+                              numericInput(inputId = "forecast", label = "How Many Months Would you Like to Forecast in the Future?", value = 24, min = 1, max = NA, step = 1,
+                                           width = NULL
+                              ),),
+                      tabItem(tabName = "ST4",
+                              checkboxGroupButtons(
+                                inputId = "basic",
+                                label = "Choices", 
+                                choices = c("NAIVE", "Seasonal NAIVE", "Drift","Linear","MEAN"),
+                                selected = "NAIVE",
+                                status = "danger"
+                              ),
+                              plotOutput(outputId = "plot4"),
+                              ),
+                      tabItem(tabName = "ST5",
+                              checkboxGroupButtons(
+                                inputId = "ETS",
+                                label = "Choices", 
+                                choices = c("Holts", "Holts Winters"),
+                                selected ="Holts",
+                                status = "danger"
+                              ),
+                              plotOutput(outputId = "plot5"),
+                              ),
+                      tabItem(tabName = "ST6",
+                              checkboxGroupButtons(
+                                inputId = "arima",
+                                label = "Choices", 
+                                choices = c("ARIMA210", "ARIMA013", "ARIMA000011", "ARIMA210011", "Stepwise","Auto","ARIMA"),
+                                selected = "ARIMA210",
+                                status = "danger"
+                              ),
+                              numericInput("p","Input p value for \"ARIMA\"",value = 1,min = 0, max = 3),
+                              br(),
+                              numericInput("d","Input d value for \"ARIMA\"",value = 1,min = 0, max = 1),
+                              br(),
+                              numericInput("q","Input p value for \"ARIMA\"",value = 1,min = 0, max = 3),
+                              plotOutput(outputId = "plot6"),
+                      ))))
+
